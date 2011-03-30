0e2dd6ffbbc4c6dd36126325153e3880f1fdae8e
Klemens Mantzos <klemens@fetzig.at>
2011-03-30 19:11:43 +0200

    updated urls.py example

--- csvexporter/urls.py
+++ csvexporter/urls.py
@@ -3,8 +3,6 @@
 
 # CSV EXPORTER
 urlpatterns += patterns('csvexporter.views',
-    url(r'^csvexporter/filter/$', 'csv_filter', kwargs={"model": User}, name='csvexporter_filter'),
-    url(r'^csvexporter/result/(?P<object_id>\d+)/$', 'csv_result', kwargs={"model": User}, name='csvexporter_result'),
-    url(r'^csvexporter/result/(?P<object_id>\d+)/csv/$', 'csv_result_csv', kwargs={"model": User}, name='csvexporter_result_csv'),
+    url(r'^csvexporter/user/filter/$', 'csv_filter', kwargs={"model": User}, name='csvexporter_filter'),
 )
 """
